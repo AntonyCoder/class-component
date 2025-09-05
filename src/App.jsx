3dffb855446dcb2196f6d01fa0083962b42899b9
Anton Lysenko <antonlysenko161@yandex.ru>
2025-09-05 20:51:55 +0300

    Реализовал классовый компонент

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import ShopItemClass from './components/ShopItemFunc/ShopItemClass';
+
+export class App extends React.Component{
+  item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
+  render() {
+    return (
+      <div className="container" >
+        <div className="background-element"></div>
+        <div className="highlight-window">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="window">
+          <ShopItemClass item={this.item} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
